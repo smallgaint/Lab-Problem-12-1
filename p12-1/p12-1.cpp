b3413ed35dc19865bc046de51439c331eb300f3e
s-hong.1 <s-hong.1@onu.edu>
2025-11-20 13:08:41 -0500

    one intermediate commit

---
+++ p12-1/p12-1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+void pressEnterToContinue();
+bool readFromFile(string filename);
+int countWords(string thisLine);
+int countChars(string thisLine);
+
+
+int main() {
+	string datafile;
+	cout << "input file: ";
+	getline(cin, datafile);
+
+	bool status = readFromFile(datafile);
+
+	pressEnterToContinue();
+}
+
+void pressEnterToContinue() {
+	cout << "\nPress enter to continue... \n";
+	cin.clear();
+	while (cin.get() != '\n') continue;
+}
+
+bool readFromFile(string filename) {
+	ifstream inFile;
+	string thisLine;
+	int wordCount = 0;
+	int charCount = 0;
+	inFile.open(filename);
+
+	if (inFile.fail()) {
+		cout << "read error - sorry\n";
+		return false;
+	}
+
+	while (true) {
+		if (inFile.fail()) break;
+		thisLine.clear();
+		getline(inFile, thisLine);
+
+		wordCount += countWords(thisLine);
+		charCount += countChars(thisLine);
+	}
+
+	cout << "Words:		" << wordCount << "\n";
+	cout << "Characters:	" << charCount << "\n";
+
+	inFile.close();
+	return true;
+}
+
+int countWords(string thisLine) {
+	int count = 0;
+
+	for (int i = 0; i < thisLine.length(); i++) {
+		if ((thisLine[i] == ' ' && !isspace(thisLine[i + 1])) || i == 0) {
+			count++;
+		}
+	}
+	cout << count << endl;
+	return count;
+}
+
+int countChars(string thisLine) {
+	int count = 0;
+
+	for (int i = 0; i < thisLine.length(); i++) {
+		if (thisLine[i] != ' ') {
+			count++;
+		}
+	}
+	return count;
+}
